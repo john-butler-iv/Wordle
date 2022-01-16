5ce532ccea4de38066a9d92c6b5fc8a2a55c7892
relicanth56 <john.m.butler.iv@gmail.com>
2022-01-16 15:14:46 -0600

    added drawWord and documentation

--- keyboard.js
+++ keyboard.js
@@ -7,6 +7,12 @@ let createKeyObj = function (letter, x, y) {
     };
 }
 
+/**
+ * determines if the new status should update the old status in <Keybaord>.keys
+ * @param {string} oldStat the status keys already contains
+ * @param {string} newStat the status from the most recent guess
+ * @returns whether the old status should replace the new status
+ */
 let shouldUpdate = function (oldStat, newStat) {
     if (oldStat === Keyboard.UNUSED) return true;
     if (oldStat === Keyboard.WRONG_SPOT && newStat === Keyboard.RIGHT_SPOT) return true;
@@ -14,12 +20,15 @@ let shouldUpdate = function (oldStat, newStat) {
 }
 
 
+/**
+ * Class that keeps track of the keyboard display
+ */
 class Keyboard {
     static UNUSED = 'u';
     static NOT_IN_WORD = 'n';
     static WRONG_SPOT = 'w';
     static RIGHT_SPOT = 'r';
-    static SPACING = 0.1
+    static SPACING = 0.10
 
     static USED_COLOR = {
         'u': '#FFFFFF',
@@ -28,35 +37,47 @@ class Keyboard {
         'r': '#00FF00'
     };
 
+    /**
+     * @param {string} word the word that's trying to be guessed
+     * @param {number} scale scaling factor to draw the keyboard at
+     * @param {number} x x-coordinate of the center of the keybaord
+     * @param {number} y y-coordinate of the center of the keyboard
+     */
     constructor(word, scale, x, y) {
         this.word = word;
         this.scale = scale;
+        // it's more useful to store (x,y) of the center of the 'Q'-key
         this.x = x - 4.5 * scale;
         this.y = y - 1 * scale;
 
 
         this.keys = [];
-        this.keysIndex = {};
+        this.keysIndex = {}; // this.keys[this.keysIndex[<letter>]] = <letter>
+
         let toprow = 'QWERTYUIOP';
         let midrow = 'ASDFGHJKL';
         let botrow = 'ZXCVBNM';
-
         for (let i = 0; i < toprow.length; i++) {
             this.keys.push(createKeyObj(toprow.charAt(i), i, 0))
             this.keysIndex[toprow.charAt(i)] = i
         }
+        // middle row is offset 1/4 a key from the top row
         for (let i = 0; i < midrow.length; i++) {
             this.keys.push(createKeyObj(midrow.charAt(i), i + 0.25, 1))
             this.keysIndex[midrow.charAt(i)] = toprow.length + i
         }
+        // bottom row is offset 1/2 a key from the middle row (3/4 from top)
         for (let i = 0; i < botrow.length; i++) {
             this.keys.push(createKeyObj(botrow.charAt(i), i + 0.75, 2))
             this.keysIndex[botrow.charAt(i)] = toprow.length + midrow.length + i
         }
     }
 
+    /**
+     * updates the keyboard's colors based on latest guess.
+     * @param {string} guessedWord the word inputted by the user
+     */
     update(guessedWord) {
-        let ret_var = [];
         for (let i = 0; i < guessedWord.length; i++) {
             let ch = guessedWord.charAt(i);
             let realCh = this.word.charAt(i);
@@ -70,9 +91,7 @@ class Keyboard {
             let keyIndex = this.keysIndex[ch];
             if (shouldUpdate(this.keys[keyIndex].status, usedVal))
                 this.keys[keyIndex].status = usedVal;
-            ret_var.push(usedVal);
         }
-        return ret_var;
     }
 
     display() {
@@ -95,6 +114,11 @@ class Keyboard {
         }
     }
 
+    /**
+     * finds the colors that each letter of word should be
+     * @param {string} word the inputted word
+     * @returns an array of colors the letters of word should be
+     */
     getColors(word) {
         let retArr = [];
         let status;
--- sketch.js
+++ sketch.js
@@ -16,6 +16,9 @@ function setup() {
     noLoop();
 }
 
+/**
+ * setup scale variables
+ */
 function setScale() {
     SCALE = windowHeight * 4 / 3;
     GUESS_SCALE = SCALE * 3 / 40;
@@ -26,6 +29,9 @@ function setScale() {
     y0 = 0.5 * GUESS_SCALE;
 }
 
+/**
+ * initializes/resets variables
+ */
 function setupCore() {
     keyboard = new Keyboard(pickWord(), KEYBOARD_SCALE, windowWidth / 2, GUESS_SCALE * 7.125);
 
@@ -42,6 +48,10 @@ function draw() {
     keyboard.display();
 }
 
+/**
+ * displays all previeous guesses, the current word, and empty squares for the future guesses. 
+ * If the player lost, then the answer is shown too.
+ */
 function displayGuesses() {
     let word, i;
     rectMode(CENTER);
@@ -49,71 +59,72 @@ function displayGuesses() {
     textAlign(CENTER, CENTER);
 
     for (i = 0; i < guesses.length; i++) {
-        word = guesses[i];
-        let colors = keyboard.getColors(word);
-        for (let j = 0; j < word.length; j++) {
-            stroke(colors[j]);
-            if (colors[j] !== Keyboard.USED_COLOR[Keyboard.NOT_IN_WORD]) {
-                fill(colors[j]);
-                rect(j * GUESS_SCALE + x0, i * GUESS_SCALE + y0, GUESS_SCALE - SPACING, GUESS_SCALE - SPACING, SPACING);
-                fill(BACKGROUND_COLOR);
-            } else {
-                noFill();
-                rect(j * GUESS_SCALE + x0, i * GUESS_SCALE + y0, GUESS_SCALE - SPACING, GUESS_SCALE - SPACING, SPACING);
-                fill(colors[j]);
-            }
-            text(word.charAt(j), j * GUESS_SCALE + x0, i * GUESS_SCALE + y0);
-        }
+        // it's technically more efficient to store the colors so I don't have to recompute every letter's color on every draw
+        // I'm having no issues with speed though, so I don't want to bother.
+        drawWord(guesses[i], x0, i * GUESS_SCALE + y0, keyboard.getColors(guesses[i]));
     }
     if (guesses.length >= 6 && guesses[guesses.length] !== keyboard.word) {
-        textSize(GUESS_SCALE / 2);
-        stroke(Keyboard.USED_COLOR[Keyboard.UNUSED]);
-        for (let j = 0; j < keyboard.word.length; j++) {
-            noFill();
-            rect(j * GUESS_SCALE + x0, 7 * GUESS_SCALE + 3 * KEYBOARD_SCALE, GUESS_SCALE - SPACING, GUESS_SCALE - SPACING, SPACING);
-            fill(Keyboard.USED_COLOR[Keyboard.UNUSED]);
-            text(keyboard.word.charAt(j), j * GUESS_SCALE + x0, 7 * GUESS_SCALE + 3 * KEYBOARD_SCALE);
-        }
+        drawWord(keyboard.word, x0, 7 * GUESS_SCALE + 3 * KEYBOARD_SCALE);
         return;
     }
 
-    //current word
-    stroke(Keyboard.UNUSED);
-    word = currWord;
-    while (word.length < 5) word += " ";
-    for (let j = 0; j < word.length; j++) {
-        noFill();
-        rect(j * GUESS_SCALE + x0, i * GUESS_SCALE + y0, GUESS_SCALE - SPACING, GUESS_SCALE - SPACING, SPACING);
-        fill(Keyboard.UNUSED);
-        text(word.charAt(j), j * GUESS_SCALE + x0, i * GUESS_SCALE + y0);
-    }
+    drawWord(currWord, x0, i * GUESS_SCALE + y0);
 
-    // remaining squares
     noFill();
-    for (let i = guesses.length + 1; i < 6; i++) {
-        for (let j = 0; j < 5; j++) {
-            rect(j * GUESS_SCALE + x0, i * GUESS_SCALE + y0, GUESS_SCALE - SPACING, GUESS_SCALE - SPACING, SPACING);
+    for (i = guesses.length + 1; i < 6; i++) {
+        drawWord('', x0, i * GUESS_SCALE + y0);
+    }
+}
+/**
+ * draws one word at (x,y) colored according to colors. If colors is not provided, or is not long enough, UNUSED will be used.
+ * If word is less than 5 letters, spaces are appended to the end of the word.
+ * @param {string} word the word to be drawn
+ * @param {number} x the x coordinate of the center of the first square
+ * @param {number} y the y corrdinate of the center of the squares
+ * @param {string[]?} colors the colors to write the letters of the word in
+ */
+function drawWord(word, x, y, colors) {
+    const UNUSED_COLOR = Keyboard.USED_COLOR[Keyboard.UNUSED];
+    const NOT_IN_WORD_COLOR = Keyboard.USED_COLOR[Keyboard.NOT_IN_WORD];
+
+    while (word.length < 5) word += ' ';
+    for (let j = 0; j < word.length; j++) {
+        let color = (colors === undefined || colors[j] === undefined) ? UNUSED_COLOR : colors[j];
+        stroke(color);
+        if (color === NOT_IN_WORD_COLOR || color === UNUSED_COLOR) {
+            noFill();
+            rect(j * GUESS_SCALE + x, y, GUESS_SCALE - SPACING, GUESS_SCALE - SPACING, SPACING);
+            fill(color);
+        } else {
+            fill(color);
+            rect(j * GUESS_SCALE + x, y, GUESS_SCALE - SPACING, GUESS_SCALE - SPACING, SPACING);
+            fill(BACKGROUND_COLOR);
         }
+        text(word.charAt(j), j * GUESS_SCALE + x, y);
     }
 }
 
+/**
+ * Handles all input
+ */
 function keyReleased() {
-    if (restart) {
-        if (key === "Enter")
+    if (restart) { // only valid input when game is finished is "Enter"
+        if (key === "Enter") // restart game
             setupCore();
-    } else if (key === "Enter" && currWord.length === 5 && isWord(currWord)) {
+    } else if (key === "Enter" && currWord.length === 5 && isWord(currWord)) { // submit word
         guesses.push(currWord);
         keyboard.update(currWord);
 
+        // end game if either won or lost
         if (currWord === keyboard.word || guesses.length === 6) restart = true;
 
         currWord = "";
     } else if (key === "Backspace" && currWord.length > 0) {
         currWord = currWord.slice(0, currWord.length - 1);
     }
-    else if (key.length === 1 && currWord.length < 5) {
+    else if (key.length === 1 && currWord.length < 5) { // text input
         let ch = key.toUpperCase().charCodeAt(0);
-        if (ch >= 65 && ch <= 90) {
+        if (ch >= 65 && ch <= 90) { // filter non-letters
             currWord += key.toUpperCase();
         }
     }
